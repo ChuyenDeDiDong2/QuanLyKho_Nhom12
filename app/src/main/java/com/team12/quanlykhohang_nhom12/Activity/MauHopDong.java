fd010048d0540b3dd984f11760274ebd876551c2
Nguyễn Gia Huy <33927550+giahuy54@users.noreply.github.com>
2020-12-04 01:17:03 +0700

    Nguyễn Gia Huy

--- app/src/main/java/com/team12/quanlykhohang_nhom12/Activity/MauHopDong.java
+++ app/src/main/java/com/team12/quanlykhohang_nhom12/Activity/MauHopDong.java
@@ -1,21 +1,55 @@
 package com.team12.quanlykhohang_nhom12.Activity;
 
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class MauHopDong implements AdapterView.OnItemSelectedListener {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-    Spinner spinnerNhaKho;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.team12.quanlykhohang_nhom12.MainActivity;
+import com.team12.quanlykhohang_nhom12.R;
 
+import java.util.ArrayList;
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+public class MauHopDong extends AppCompatActivity {
 
+    private Spinner spinnerNhaKho;
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    private ArrayList<String> arrayList = new ArrayList<>();
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mau_hopdong);
+
+        spinnerNhaKho = findViewById(R.id.spinnerNhaKho);
+        showDataSpinner();
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
+    private void showDataSpinner() {
+        databaseReference.child("KhoHang").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                arrayList.clear();
+                for(DataSnapshot item : snapshot.getChildren()){
+                    arrayList.add(item.child("KhoHang").getValue(String.class));
+                }
 
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MauHopDong.this,R.layout.style_spinner,arrayList);
+                spinnerNhaKho.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/team12/quanlykhohang_nhom12/Activity/SpinnerActivity.java
+++
@@ -1,7 +0,0 @@
-package com.team12.quanlykhohang_nhom12.Activity;
-
-import android.app.Activity;
-
-public class SpinnerActivity extends Activity {
-
-}
