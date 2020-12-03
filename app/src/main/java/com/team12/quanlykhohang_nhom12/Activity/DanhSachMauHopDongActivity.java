b96078ee957c3a04969d8d8b751a0781520dd342
Nguyen Viet Thuan <nvt2491997@gmail.com>
2020-12-03 08:36:11 +0700

    DanhSachMauHopDongActivity.java them mauhopdong_item.xml

---
+++ app/src/main/java/com/team12/quanlykhohang_nhom12/Activity/DanhSachMauHopDongActivity.java
@@ -0,0 +1,14 @@
+package com.team12.quanlykhohang_nhom12.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DanhSachMauHopDongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danh_sach_mau_hop_dong);
+    }
+}
---
+++ app/src/main/java/com/team12/quanlykhohang_nhom12/Activity/MauHopDong.java
@@ -0,0 +1,6 @@
+package com.team12.quanlykhohang_nhom12.Activity;
+
+public class MauHopDong {
+
+    private String masohopdong
+}
