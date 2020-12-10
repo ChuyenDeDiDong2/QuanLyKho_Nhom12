4379942c1da750ac9becbfc2bcbde18b8aca4167
Nguyen Viet Thuan <nvt2491997@gmail.com>
2020-12-10 07:44:00 +0700

    Nguyễn Viết Thuận - sừa thêm hàng hóa

--- app/src/main/java/com/team12/quanlykhohang_nhom12/Activity/NewProductActivity.java
+++ app/src/main/java/com/team12/quanlykhohang_nhom12/Activity/NewProductActivity.java
@@ -6,35 +6,66 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.team12.quanlykhohang_nhom12.Adapter.HangHoaAdapter;
 import com.team12.quanlykhohang_nhom12.R;
 
 public class NewProductActivity extends AppCompatActivity {
-    Toolbar toolbar;
+    Button btnThem;
+    Button btnCH;
+    ImageView imgHH;
+    EditText edtTenHang, edtSoLuong, edtDonVi;
+    HangHoaAdapter adapter;
+    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_product);
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        setControls();
+        //them
+        btnThem = (Button) findViewById(R.id.btnThem);
+        edtTenHang = (EditText) findViewById(R.id.edtTenhang);
+        edtSoLuong = (EditText) findViewById(R.id.edtSoLuong);
+        edtDonVi = (EditText) findViewById(R.id.edtDonVi);
+        //test
+        btnCH = (Button) findViewById(R.id.btnCH);
+        imgHH = (ImageView) findViewById(R.id.imgHH);
+
+
+        adapter = new HangHoaAdapter();
+        reference = FirebaseDatabase.getInstance().getReference();
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String TenHangHoa = edtTenHang.getText().toString();
+                String SoLuong = edtSoLuong.getText().toString();
+                String DonVi = edtDonVi.getText().toString();
+                HangHoaAdapter adapter;
+                adapter = new HangHoaAdapter(TenHangHoa, SoLuong, DonVi);
+                reference.child("HangHoa").push().setValue(adapter);
+
+                Toast.makeText(NewProductActivity.this, "Thêm hàng hóa thành công", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //
+        btnCH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestPermissions();
+            }
+        });
     }
 
-    private void setControls() {
+    //
+    private void requestPermissions(){
 
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Thêm nhân viên");
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:  // Sự kiện nút back
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/team12/quanlykhohang_nhom12/Fragment/StaftManagerFragment.java
+++ app/src/main/java/com/team12/quanlykhohang_nhom12/Fragment/StaftManagerFragment.java
@@ -32,7 +32,5 @@ public class StaftManagerFragment extends Fragment {
         });
 
         return root;
-
-
     }
 }
