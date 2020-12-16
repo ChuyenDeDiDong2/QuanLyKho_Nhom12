817900916a66db87a5b5cdcc16f83d163f1b258e
Nguyễn Gia Huy <33927550+giahuy54@users.noreply.github.com>
2020-12-16 09:32:39 +0700

    Nguyễn Gia Huy _ Xóa các file liên quan đến mẫu hợp đồng Lý do: Không còn phù hợp với đồ án

--- app/src/main/java/com/team12/quanlykhohang_nhom12/MainActivity.java
+++ app/src/main/java/com/team12/quanlykhohang_nhom12/MainActivity.java
@@ -1,23 +1,18 @@
 package com.team12.quanlykhohang_nhom12;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
-import com.team12.quanlykhohang_nhom12.Activity.AddStaftActivity;
-import com.team12.quanlykhohang_nhom12.Activity.HomeActivity;
-import com.team12.quanlykhohang_nhom12.Activity.HomeToRentActivity;
-import com.team12.quanlykhohang_nhom12.Activity.LoginActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.team12.quanlykhohang_nhom12.Activity.DangNhapActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        Intent intent = new Intent(MainActivity.this, DangNhapActivity.class);
         startActivity(intent);
 
     }
