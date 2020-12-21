3339c5653f1d6c59a4539ce5b2ceb119be61883c
LeQuocDung <37360775+LeQuocDung@users.noreply.github.com>
2020-12-21 11:10:36 +0700

    giao diện adminapp
    giao diện adminapp

---
+++ app/src/main/java/com/team12/quanlykhohang_nhom12/Fragment/HomeAdminAppFragment.java
@@ -0,0 +1,23 @@
+package com.team12.quanlykhohang_nhom12.Fragment;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.team12.quanlykhohang_nhom12.R;
+
+public class HomeAdminAppFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_home_admin_app_fragment, container, false);
+
+        return root;
+    }
+}
